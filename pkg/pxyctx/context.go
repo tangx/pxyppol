de9b717ce3ac3d90d09b900a833bf90d0321f79f
tangx <uyinn@live.com>
2021-01-15 14:45:05 +0800

    delete unuse init

--- pkg/pxyctx/context.go
+++ pkg/pxyctx/context.go
@@ -1,10 +1,5 @@
 package pxyctx
 
-import (
-	"fmt"
-	"time"
-)
-
 var (
 	PxyCandidateCh = make(chan string, 100)
 	PxyReadyCh     = make(chan string, 100)
@@ -18,22 +13,3 @@ type Proxy struct {
 }
 
 var Pool = make(map[string]bool)
-
-func init() {
-	Pool["http://127.0.0.1:7890"] = true
-	Pool["socks5://127.0.0.1:7890"] = true
-}
-
-func init() {
-	// go counter()
-}
-
-func counter() {
-
-	for {
-		time.Sleep(1 * time.Second)
-		fmt.Println("PxyCandidateCh", len(PxyCandidateCh))
-		fmt.Println("PxyReadyCh", len(PxyReadyCh))
-		fmt.Println("PxyExpiredCh", len(PxyExpiredCh))
-	}
-}
